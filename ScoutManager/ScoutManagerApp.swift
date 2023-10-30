dfba89f2c40829fab6e99aa6ecaeaac9978c3e0b
Eugenio Tampieri <eugenio@eutampieri.eu>
2023-10-30 09:58:26 +0100

    Initial Commit

---
+++ ScoutManager/ScoutManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutManagerApp.swift
+//  ScoutManager
+//
+//  Created by Eugenio Tampieri on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ScoutManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
